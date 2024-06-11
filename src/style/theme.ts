819d774d32f2e57636bf86a5505384f199e060a2
khil94 <zidru94@gmail.com>
2024-06-12 00:38:12 +0900

    style : styleprovider 필요없는 값들 삭제 및 색 재설정

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,42 +1,20 @@
 import { palette } from "./palette";
 export const lightTheme = {
-  main: "#f2f2f2",
-  contrast: "#1f2e3d",
+  background1: "#ffffff",
+  background2: "#3A536D",
 
-  background1: "#8F979E",
-  background2: "#a4aab0",
-  background3: "#b9bec2",
-  background4: "#ced2d5",
+  border: "#d2d4d6",
 
-  border1: "#eaeaed",
-  border2: "#dee2e6",
-  border3: "#b5b5b5",
-  border4: "#343a40",
-
-  text1: "#626D77",
-  text2: "#414D5A",
-  text3: "#1B2937",
-  text4: "#121D27",
+  text1: "#1f2d28",
 };
 
 export const darkTheme = {
-  main: "#1f2e3d",
-  contrast: "#f2f2f2",
+  background1: "#0d1117",
+  background2: "#24292e",
 
-  background1: "#626D77",
-  background2: "#414D5A",
-  background3: "#1B2937",
-  background4: "#121D27",
+  border: "#515961",
 
-  border1: "#2a2a2a",
-  border2: "#4d4d4d",
-  border3: "#a0a0a0",
-  border4: "#e0e0e0",
-
-  text1: "#8F979E",
-  text2: "#BCC0C5",
-  text3: "#E4E6E8",
-  text4: "#f9f9f9",
+  text1: "#e6edf3",
 };
 
 export type IPaletteTheme = typeof darkTheme & typeof palette;
