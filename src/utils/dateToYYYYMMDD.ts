d9668af910c0744a0c72350acd4bbf8e9a7c0db3
khil94 <zidru94@gmail.com>
2024-06-12 02:10:17 +0900

    feat : date string을 받아 YYYYMMDD로 반환하는 함수

---
+++ src/utils/dateToYYYYMMDD.ts
@@ -0,0 +1,11 @@
+export function dateToYYYYMMDD(date: string) {
+  const newDate = new Date(date);
+
+  return `${newDate.getFullYear()}년 ${makeZeroNumber(
+    newDate.getMonth()
+  )}월 ${makeZeroNumber(newDate.getDay())}일`;
+}
+
+export function makeZeroNumber(target: number) {
+  return `${target > 10 ? target : "0" + target}`;
+}
