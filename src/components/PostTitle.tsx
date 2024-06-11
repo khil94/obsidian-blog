f36e9ec3fc0f2493ef020262bd1241ca2686dd40
khil94 <zidru94@gmail.com>
2024-06-12 02:10:34 +0900

    feat : PostTittle Component 초안

--- src/components/PostTitle.tsx
+++ src/components/PostTitle.tsx
@@ -1,14 +1,31 @@
+import { dateToYYYYMMDD } from "@/utils/dateToYYYYMMDD";
 import { styled } from "styled-components";
+import Flex from "./Flex";
+import Text from "./Text";
 
 interface Iprop {
   content: string;
+  createdAt: string;
 }
-export default function PostTitle({ content }: Iprop) {
-  return <PostTitleWrapper>{content}</PostTitleWrapper>;
+export default function PostTitle({ content, createdAt }: Iprop) {
+  return (
+    <PostTitleWrapper
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Text wordBreak="keep-all" tagName="h1" fontSize="4rem" lineHeight="1">
+        {content}
+      </Text>
+      <Flex width="100%" justifyContent="flex-end">
+        <Text>{dateToYYYYMMDD(createdAt)}</Text>
+      </Flex>
+    </PostTitleWrapper>
+  );
 }
 
-const PostTitleWrapper = styled.div`
-  // background-color: ${({ theme }) => theme.palette.background1};
-  color: ${({ theme }) => theme.palette.black};
-  padding: 24px;
+const PostTitleWrapper = styled(Flex)`
+  background-color: ${({ theme }) => theme.palette.background1};
+  color: ${({ theme }) => theme.palette.text1};
+  margin: 2rem 0;
 `;
