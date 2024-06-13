cc5557528a98af0b5274461e2865012245bd7146
khil94 <zidru94@gmail.com>
2024-06-13 21:49:41 +0900

    feat : Navbar 목업

---
+++ src/containers/Navbar.tsx
@@ -0,0 +1,26 @@
+import Button from "@/components/Button";
+import Flex from "@/components/Flex";
+import Text from "@/components/Text";
+import useTheme from "@/hooks/useTheme";
+import Link from "next/link";
+
+export default function Navbar({ children }: { children?: React.ReactNode }) {
+  const { toggleTheme } = useTheme();
+
+  return (
+    <Flex
+      padding="1rem"
+      justifyContent="space-between"
+      alignItems="center"
+      tagName="nav"
+    >
+      <Link href="/">
+        <Flex>
+          <img width={36} src="/logo.png" alt="logo" />
+          <Text margin="0 1rem">Zidru's blog</Text>
+        </Flex>
+      </Link>
+      <Button onClick={toggleTheme}>ON</Button>
+    </Flex>
+  );
+}
