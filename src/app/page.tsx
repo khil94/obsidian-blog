ed62109159546b9b597b548d39e4cb50b6ec5d51
khil94 <zidru94@gmail.com>
2024-06-13 21:22:45 +0900

    chore : 홈 화면 console값 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,6 @@ export default function Home() {
   const posts = allPosts.sort((a, b) =>
     compareDesc(new Date(a.createdAt), new Date(b.createdAt))
   );
-
-  console.log(posts[0].body.code);
-  const Test = useMDXComponent(posts[0].body.code);
-  console.log([posts[0].body]);
   return (
     <div className="mx-auto max-w-xl py-8">
       <h1 className="mb-8 text-center text-2xl font-black"></h1>
