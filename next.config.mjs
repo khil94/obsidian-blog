f6bcff32b30fbff0113a84cc4ee817a01b3e562c
khil94 <zidru94@gmail.com>
2024-06-16 02:48:16 +0900

    feat : svg를 컴포넌트 처럼 쓰기위한 svgr추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.(js|ts)x?$/,
+
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
 
 export default withContentlayer(nextConfig);
