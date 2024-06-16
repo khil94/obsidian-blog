cf1eaf516c2bdc000280f128dd1e190d08a6ed38
khil94 <zidru94@gmail.com>
2024-06-17 04:58:30 +0900

    feat : categoryList 관련 유틸 변수
    post의 categoryList와 category에 따라 post를 정리한 Record타입 변수 정의

---
+++ src/utils/category.ts
@@ -0,0 +1,10 @@
+import { Post, allPosts } from "@/contentlayer/generated";
+
+export const categoryList = new Set(allPosts.map((v) => v.category));
+
+type IPostListByCategory = Record<string, Post[]>;
+
+export const PostListByCategory = allPosts.reduce((prev, curr) => {
+  prev[curr.category] = [...(prev[curr.category] || []), curr];
+  return prev;
+}, {} as IPostListByCategory);
