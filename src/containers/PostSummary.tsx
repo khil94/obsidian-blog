3e3cf66cf92a8f612394da5de3a249846853c33d
khil94 <zidru94@gmail.com>
2024-06-18 16:52:55 +0900

    style : PostSummary 스타일링
    PostSummary의 description의 최소 높이 지정, category도 표기, 각 정보 표기순서 조정

--- src/containers/PostSummary.tsx
+++ src/containers/PostSummary.tsx
@@ -2,6 +2,7 @@ import Anchor from "@/components/Anchor";
 import Flex from "@/components/Flex";
 import Text from "@/components/Text";
 import { dateToYYYYMMDD } from "@/utils/dateToYYYYMMDD";
+import makeEllipsis from "@/utils/makeEllipsis";
 import { styled } from "styled-components";
 
 interface IProp {
@@ -10,6 +11,7 @@ interface IProp {
   thumbnail?: string;
   url: string;
   date: string;
+  category: string;
 }
 
 export default function PostSummary({
@@ -18,19 +20,30 @@ export default function PostSummary({
   thumbnail,
   url,
   date,
+  category,
 }: IProp) {
   return (
-    <Anchor href={url}>
-      <PostSummaryWrapper borderRadius="8px">
+    <Anchor width="36rem" href={url}>
+      <PostSummaryWrapper gap=".5rem" padding="1.2rem" borderRadius="8px">
         <img src={thumbnail} alt="thumbnail" />
-        <Flex backgroundColor="none" padding="2.4rem" flexDirection="column">
-          <Text tagName="h2" fontSize="1.5rem" fontWeight="bold">
-            {title}
-          </Text>
+        <Flex
+          width="100%"
+          backgroundColor="none"
+          flexDirection="column"
+          gap=".5rem"
+          flex="1"
+        >
           <Text fontSize=".8rem" textAlign="right">
             {dateToYYYYMMDD(date)}
           </Text>
-          <Text>{description}</Text>
+          <Text maxLine={2} tagName="h2" fontSize="1.5rem" fontWeight="bold">
+            {title}
+          </Text>
+
+          <Text minHeight="3.5rem" maxLine={2}>
+            {makeEllipsis(description, 83)}
+          </Text>
+          <PostSummaryCategory fontSize=".8rem">{category}</PostSummaryCategory>
         </Flex>
       </PostSummaryWrapper>
     </Anchor>
@@ -39,4 +52,8 @@ export default function PostSummary({
 
 const PostSummaryWrapper = styled(Flex)`
   border: 1px ${({ theme }) => theme.palette.border} solid;
+`;
+
+const PostSummaryCategory = styled(Text)`
+  color: ${({ theme }) => theme.palette.main};
 `;
