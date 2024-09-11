d89fccf7c8527c786263db7eb79a058a0806c130
khil94 <zidru94@gmail.com>
2024-09-11 14:05:20 +0900

    feat : loading comp + loading 사전 컴포넌트 추가

---
+++ src/app/post/[idx]/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/components/Loading";
+
+export default function PostLoading() {
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
+}
---
+++ src/components/Loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <img src="/spinner.vg" width={50} height={50} />;
+}
