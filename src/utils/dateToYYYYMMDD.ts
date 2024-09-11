b50a28e0f67e17a3e475a3da237b7edb311c9ffe
khil94 <zidru94@gmail.com>
2024-09-11 13:45:33 +0900

    fix : getMonth에 +1 안하던 문제 수정

--- src/utils/dateToYYYYMMDD.ts
+++ src/utils/dateToYYYYMMDD.ts
@@ -2,7 +2,7 @@ export function dateToYYYYMMDD(date: string) {
   const newDate = new Date(date);
 
   return `${newDate.getFullYear()}년 ${makeZeroNumber(
-    newDate.getMonth()
+    newDate.getMonth() + 1
   )}월 ${makeZeroNumber(newDate.getDate())}일`;
 }
 
