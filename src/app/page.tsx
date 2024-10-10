89c7048bc21004a1dfbed214ffde1f37d5023fa0
khil94 <zidru94@gmail.com>
2024-10-11 01:01:51 +0900

    vault backup: 2024-10-11 01:01:51

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,4 +3,15 @@ import Home from "@/containers/Home";
 
 export default function HomePage() {
   return <Home />;
+  // return (
+  //   <Flex
+  //     style={{
+  //       height: "calc(100vh - 9rem)",
+  //       alignItems: "center",
+  //       justifyContent: "center",
+  //     }}
+  //   >
+  //     <Loading />
+  //   </Flex>
+  // );
 }
