4ff70ad54a652d66eef3e8e19866a86b69e4497e
khil94 <zidru94@gmail.com>
2025-01-14 18:01:50 +0900

    feat : 누락코드 / PostListByCategory를 단순 filter 함수로 변경

--- src/utils/category.ts
+++ src/utils/category.ts
@@ -4,7 +4,11 @@ export const categoryList = new Set(allPosts.map((v) => v.category));
 
 type IPostListByCategory = Record<string, Post[]>;
 
-export const PostListByCategory = allPosts.reduce((prev, curr) => {
-  prev[curr.category] = [...(prev[curr.category] || []), curr];
-  return prev;
-}, {} as IPostListByCategory);
+// export const PostListByCategory = allPosts.reduce((prev, curr) => {
+//   prev[curr.category] = [...(prev[curr.category] || []), curr];
+//   return prev;
+// }, {} as IPostListByCategory);
+
+export const PostListByCategory = (postList: Post[], category: string) => {
+  return postList.filter((v) => v.category === category);
+};
